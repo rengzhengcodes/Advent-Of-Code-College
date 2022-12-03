1eaaf22eff88a08930f1e65fdba6778a90342d43
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-12-02 21:25:37 -0500

    copy broken

--- Python/2022/1/main.py
+++ Python/2022/1/main.py
@@ -1,9 +1,22 @@
 # allows import of advent_io
 import sys
 sys.path.append("../../")
-import advent_io
+from advent_io import *
 
-advent_io.fetch_input(1, 2022)
+fetch_input(1, 2022)
 
 def soln_1():
+    # preprocessing on file to isolate elfs
+    with open("input.txt") as file:
+        raw = file.read() # all the data
+        elf_isolation = raw.split("\n\n") # double lines split elfs
+        elf_isolation = [string.split('\n') for string in elf_isolation] # splits the calories into separate strings
+        
+        # converts all strings into ints
+        for elf in range(len(elf_isolation)):
+            elf_isolation[elf] = (int(string) for string in elf)
+        
+        # totals all calories per elf
+        total_cals = [sum(elf) for elf in elf_isolation]
     
+    copy_ans(str(max(total_cals)))
