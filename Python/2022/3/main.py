2fcbe2e19e4ecfd5d21340b26a2fd00ee203ab7d
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-12-05 13:28:48 -0500

    day 3 complete

--- Python/2022/3/main.py
+++ Python/2022/3/main.py
@@ -4,3 +4,50 @@ sys.path.append("../../")
 from advent_io import *
 
 fetch_input(3, 2022)
+
+def soln_1():
+    with open("input.txt", 'r') as file:
+        sacks = file.read().rstrip()
+        sacks = sacks.split('\n')
+        # splits between the two components
+        sacks = [(sack[0 : len(sack) // 2], sack[len(sack) // 2:]) for sack in sacks]
+    
+    priorities = 0
+    for sack in sacks:
+        common_char = list(set(sack[0]) & set(sack[1]))[0]
+        if common_char.isupper():
+            # converts to ascii, decrements to get position of letter
+            priorities += ord(common_char) - 64
+            # 26 mor epoints b/c upper
+            priorities += 26
+        else:
+            # converts to ascii, decrements to get position of letter
+            priorities += ord(common_char) - 96
+
+    print(priorities)
+    copy_ans(priorities) 
+
+# soln_1()
+
+def soln_2():
+    with open("input.txt", 'r') as file:
+        sacks = file.read().rstrip()
+        sacks = sacks.split('\n')
+        groups = [(sacks[i], sacks[i + 1], sacks[i + 2]) for i in range(0, len(sacks), 3)]
+    
+    priorities = 0
+    for group in groups:
+        common_char = list(set(group[0]) & set(group[1]) & set(group[2]))[0]
+        if common_char.isupper():
+            # converts to ascii, decrements to get position of letter
+            priorities += ord(common_char) - 64
+            # 26 mor epoints b/c upper
+            priorities += 26
+        else:
+            # converts to ascii, decrements to get position of letter
+            priorities += ord(common_char) - 96
+
+    print(priorities)
+    copy_ans(priorities) 
+
+soln_2()
