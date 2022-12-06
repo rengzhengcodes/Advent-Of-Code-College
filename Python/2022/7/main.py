4aedf3d9c92db7d02de5d20cacd4c8773443efe2
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-12-06 03:29:32 -0500

    boilerplate for day 7

---
+++ Python/2022/7/main.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("../../")
+from advent_io import *
+
+# imports a stack-like object
+from collections import deque
+# import pandas and StringIO to feed data into pandas
+from io import StringIO
+from time import sleep
+import numpy as np
+import pandas as pd
+import regex as re
+
+fetch_input(7, 2022)
