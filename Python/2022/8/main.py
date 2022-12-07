435bfdb778e0a92bbeb3b7e5105a8c86b7916849
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-12-07 09:05:46 -0500

    day 8 boiler

---
+++ Python/2022/8/main.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("../../")
+from advent_io import *
+
+# imports a stack-like object
+from collections import deque
+# import pandas and StringIO to feed data into pandas
+from io import StringIO
+from time import sleep
+import numpy as np
+import pandas as pd
+import regex as re
+
+fetch_input(8, 2022)
+
+def soln_1():
+    with open("input.txt", 'r') as file:
+        raw = file.read().rstrip()
+
+soln_1()
+
+def soln_2():
+    pass
+
+soln_2()
