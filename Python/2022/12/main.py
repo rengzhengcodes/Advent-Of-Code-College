e5c624a34a16474d28577d4d5bced0e63e676d0b
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-12-11 20:12:25 -0500

    changes to boiler

--- Python/2022/12/main.py
+++ Python/2022/12/main.py
@@ -5,19 +5,27 @@ from advent_io import *
 # type hinting
 from typing import Union
 from numbers import Number
-# imports iterable type
+# imports iterable type for type hinting
 from collections.abc import Iterable
+
 # imports a stack-like object
 from collections import deque
+
 # import pandas and StringIO to feed data into pandas
 from io import StringIO
-from time import sleep
-import numpy as np
 import pandas as pd
 import regex as re
+
+# imports numpy for np.ndarray abuse
+import numpy as np
+
+# imports sleep in case we want to do step-by-step prints
+from time import sleep
+
 # imports add for map
 from operator import add
-# removes indent from multiline strings
+
+# imports dedent to removes indent from multiline strings
 from textwrap import dedent
 
 fetch_input(12, 2022)
