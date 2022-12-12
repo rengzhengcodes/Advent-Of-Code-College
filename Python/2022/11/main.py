2f5dfb54b2b79db791590e2f15389d42e01af020
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-12-11 20:01:52 -0500

    Day 11 part 2 code commented

--- Python/2022/11/main.py
+++ Python/2022/11/main.py
@@ -189,15 +189,32 @@ def soln_2():
     with open("input.txt", 'r') as file:
         # raw file
         raw:str = file.read().rstrip()
-        # isolates each monkey
-        raw = raw.replace('\n\n', '\n')
+        # gets rid of extraneous whitespace between monkeys for parsing
+        raw:str = raw.replace('\n\n', '\n')
+        # isolates each line of the input so we can parse the monkeys
         monkeys_text = [line.rstrip() for line in raw.split("\n")]
     
     # class for each monkey
     class Monkey():
         # CRT = Chinese Remainder Theorem number for keeping worry manageable
         crt:int = 1
-        def __init__(self, name:str, starting_items:list, operation:str, test:str, test_results:tuple):
+
+        def __init__(self, name:str, starting_items:list, operation:str, test:str, test_results:tuple) -> None:
+            """
+            Creates a monkey class meant to simulate monkey behavior in AoC Day 11
+
+            name:str
+                The monkey's identifier
+            starting_items:list
+                The items the monkey starts with (list of strings that represent integers, representing you're worry)
+            operation:str
+                What happens to your worry for an item while the monkey is inspecting it
+            test:str
+                How the monkey decides what to do with an item based on how worried you are about it
+            test_results:tuple
+                The decisions the monkey can make, dependent on the result of test
+            """
+
             # name of the monkey
             self.name:str = name
             # items the monkey currently possesses
@@ -211,9 +228,12 @@ def soln_2():
             # what the monkey does after it does its test
             self.test_results:tuple = tuple([int(test_result.split(' ')[-1]) for test_result in test_results])
             # amount of inspections conducted
-            self.inspections = 0
+            self.inspections:int = 0
         
-        def __str__(self):
+        def __str__(self) -> str:
+            """
+            Tries to create an output string as close as possible to the input to check monkeys are initialized correctly
+            """
             return dedent(f"""
             {self.name}:
                 Starting items: {self.items}
@@ -222,24 +242,33 @@ def soln_2():
                     Results: {self.test_results}
             """)
         
-        def monkey_business(self, monkey_list:list):
+        def monkey_business(self, monkey_list:list) -> None:
             """
-            Runs the monkey
+            Runs the monkey as it does monkey business.
+
+            monkey_list:list
+                The list of monkeys in the system (indexed by the numeric in their name)
             """
+
             # updates inspections, but before they're run as the # of inspections is predictable
-            inspections = len(self.items)
+            inspections:int = len(self.items)
             self.inspections += inspections
             # goes through each item
+            i:int
             for i in range(inspections):
                 ### CONDUCTS OPERATION ###
+                # gets the old worry value
                 old:int = self.items[i]
+                # gets the operation's literal value (or detects if it's using old)
                 match(self.operation[-1]):
                     case('old'):
-                        value = old
+                        value:int = old
                     case _:
-                        value = int(self.operation[-1])
-            
-                new = 0
+                        value:int = int(self.operation[-1])
+
+                # new worry value
+                new:int = 0
+                # operation parsing for new worry value
                 match (self.operation[-2]):
                     case('+'):
                         new = old + value
@@ -283,6 +312,8 @@ def soln_2():
         return:int
             the index of the line in the string_list we got to
         """
+
+        # goes through each line in the input
         while (index < len(string_list)):
             # the parts of the line split up
             line:tuple = tuple([part.rstrip().strip() for part in string_list[index].split(':')])
@@ -295,25 +326,46 @@ def soln_2():
             
             # if a line's only attribute is itself and an indented descriptor, make a dict corresponding the two
             elif len(line) == 1:
-                # creates indentation block dictionary
+                # creates indentation block dictionary, as this always means it's an indentation block coming and its description is just the indentation block
                 current_level[line[0]] = dict()
-                # fills out indentation block dictionary, sets index to where the last operation left off
+
+                """
+                fills out indentation block dictionary, sets index to where the last operation left off.
+                index should update to the line where the recursive ended so the next iteration of the loop
+                can parse the line where the indentation ended.
+                index=index + 1 for the input as we already read the line for the monkey and thus we want the subdescriptor
+                to start after the monkey declaration line
+                """
                 index = build_monkeys_dict(string_list, current_level[line[0]], index=index + 1, indentation_level=indentation)
+            # nested descriptor detection
             elif indentation > indentation_level:
-                # previous line was actually a nested descriptor
+                """
+                previous line was actually a nested descriptor, 
+                reparse and make the previous line into a dictionary 
+                with descriptor (in-line attribute) and sub descriptors 
+                (indented attributes)
+                """
+                # grab previous line
                 prev_line:str = string_list[index - 1]
+                # parse previous line like normal
                 prev_line:tuple = tuple([part.rstrip().strip() for part in prev_line.split(':')])
+                # dictionary representing subdescriptors
                 sub_desc = dict()
+                # redefines the previous line to the dictionary described in the multiline comment
                 current_level[prev_line[0]] = {
                     'desc': prev_line[1],
                     'sub_desc': sub_desc
                 }
                 # builds sub_desc
                 index = build_monkeys_dict(string_list, sub_desc, index=index, indentation_level=indentation)
+            # inline data parsing
             else:
+                # defines inline data
                 current_level[line[0]] = line[1]
+                # the only time we actually want to parse a line after, so the only time we do index++
                 index += 1
         
+        # to keep in line with the other return values in case the while ends without returning
         return index
 
     # dictionary of all monkeys by name
@@ -321,22 +373,19 @@ def soln_2():
     
     # makes the monkeys into dictionarys
     build_monkeys_dict(monkeys_text, monkeys)
-    # print(monkeys)
 
-    # objectify monkeys
+    # objectify monkeys based on their dictionary entry
     monkey_list:list = list()
     for name, attribute in monkeys.items():
         attribute = attribute['sub_desc']
         monkey_list.append(Monkey(name, [int(item) for item in attribute['Starting items'].split(", ")], attribute['Operation'], attribute['Test']['desc'], tuple(attribute['Test']['sub_desc'].values())))
-    
-    # calculates the modulus number to keep worry manageable
-    Monkey
 
-
+    # goes through all 10000 rounds for soln_2
     for i in range(10000):
         for monkey in monkey_list:
             monkey.monkey_business(monkey_list)
 
+    # pulls out and sorts monkey activity to calculate monkey business
     monkey_activity:list = [monkey.inspections for monkey in monkey_list]
     monkey_activity.sort()
     monkey_business = monkey_activity[-2] * monkey_activity[-1]
