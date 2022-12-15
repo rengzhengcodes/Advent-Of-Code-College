174558e05b68b31d5ac11ccd452d9b660feccd26
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-12-15 16:58:33 -0500

    Day 15 part 1 solved

--- Python/2022/15/main.py
+++ Python/2022/15/main.py
@@ -38,5 +38,110 @@ def soln_1():
     with open("input.txt", 'r') as file:
         # raw file
         raw:str = file.read().rstrip()
+        raw = raw.split('\n')
+        # splits to sensor, beacon pairs
+        pairs:list = [list(line.split(": ")) for line in raw]
+        pairs = [[sensor.split(" at ")[1].split(", "), beacon.split(" at ")[1].split(", ")] for sensor, beacon in pairs]
+        # extracts sensor and beacon into integer base pairs
+        pairs = [
+                    (
+                        tuple([int(val.split('=')[1]) for val in sensor]),
+                        tuple([int(val.split('=')[1]) for val in beacon])
+                    )
+                    for sensor, beacon in pairs
+                ]
+    
+    # finds map bounds
+    x_vals:list = [elem[0] for pair in pairs for elem in pair]
+    y_vals:list = [elem[1] for pair in pairs for elem in pair]
+    x_bounds:tuple = (min(x_vals), max(x_vals))
+    y_bounds:tuple = (min(y_vals), max(y_vals))
+
+    def calc_manhattan_dist(loc0:tuple, loc1:tuple) -> int:
+        """
+        Calculates the manhattan distance between 2 locations
+
+        loc1:tuple
+            Position 1
+        loc2:tuple
+            Position 2
+        
+        Returns:
+        Manhattan distance
+        """
+        manhattan_distance:int = sum([abs(loc0[i] - loc1[i]) for i in range(len(loc0))])
+        return manhattan_distance
+    
+    # corresponds sensor to its maximum distance given nearest beacon
+    sensor_max_dist:dict = dict()
+    for sensor, beacon in pairs:
+        # notes distance from each sensor to closest beacon
+        sensor_max_dist[sensor] = calc_manhattan_dist(sensor, beacon)
+    
+    y = 2000000
+    # calculates number of no beacons in y=2000000
+    no_beacons:int = 0
+    for x in range(x_bounds[0], x_bounds[1] + 1):
+        empty:bool = True
+        current_location:tuple = (x, y)
+
+        for sensor, max_dist in sensor_max_dist.items():
+            if calc_manhattan_dist(sensor, current_location) <= max_dist:
+                empty = False
+                break
+        
+        if not empty:
+            no_beacons += 1
+    
+    """
+    # counts the area before the x_bound. 
+    # The x_bound represents the entity object limit, 
+    # so with the way things are set up we only need to
+    # calculate past the bounds so long as no beacon can be there
+    """
+    # if it's possible for a beacon to be there
+    possible:bool = False
+
+    x = x_bounds[0] - 1
+    while(not possible):
+        possible = True
+        current_location:tuple = (x, y)
+        for sensor, max_dist in sensor_max_dist.items():
+            if calc_manhattan_dist(sensor, current_location) <= max_dist:
+                possible = False
+                break
+        
+        if not possible:
+            no_beacons += 1
+            x -= 1
+    
+    """
+    Counts area after the x bound for similar logic
+    """
+    # if it's possible for a beacon to be there
+    possible:bool = False
+
+    x = x_bounds[1] + 1
+    while(not possible):
+        possible = True
+        current_location:tuple = (x, y)
+        for sensor, max_dist in sensor_max_dist.items():
+            if calc_manhattan_dist(sensor, current_location) <= max_dist:
+                possible = False
+                break
+        
+        if not possible:
+            no_beacons += 1
+            x += 1
+
+    # removes beacons from full
+    counted_beacons = set()
+    for signal, beacon in pairs:
+        if beacon not in counted_beacons and beacon[1] == y:
+            counted_beacons.add(beacon)
+            no_beacons -= 1
+
+    print(no_beacons)
+    copy_ans(no_beacons)
 
 soln_1()
