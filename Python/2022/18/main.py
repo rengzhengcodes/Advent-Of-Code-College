0f9430cfbc15722c7d6223a65022ad5a4eb3d77f
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-12-21 22:01:21 -0500

    Day 18 part 2 done

--- Python/2022/18/main.py
+++ Python/2022/18/main.py
@@ -93,7 +93,7 @@ def soln_1():
 
 def soln_2():
     # parses input
-    with open("example.txt", 'r') as file:
+    with open("input.txt", 'r') as file:
         # raw file
         raw:str = file.read().rstrip()
         # cube center string representation
@@ -146,72 +146,85 @@ def soln_2():
         y_coords.append(cube[1])
         z_coords.append(cube[2])
 
-    # coords are all positive numbers so lower_bounds is just origin. ALSO! It means it's unoccupied
-    lower_bounds:tuple = (0, 0, 0)
+    lower_bounds:tuple = (min(x_coords) - 1, min(y_coords) - 1, min(z_coords) - 1)
     upper_bounds:tuple = (max(x_coords) + 1, max(y_coords) + 1, max(z_coords) + 1)
 
     del x_coords, y_coords, z_coords
 
     # cellular automata to find touchable surfaces
-    visited:set = set()
-    touched:set = set()
     def water_flow(
-        position:tuple, lower_bounds:tuple = lower_bounds, surfaces:set=surfaces,
-        upper_bounds:tuple=upper_bounds, visited:set=visited, 
-        touched:set=touched
-    ):
+        start:tuple, lower_bounds:tuple = lower_bounds, 
+        surfaces:set=surfaces, upper_bounds:tuple=upper_bounds
+    ) -> int:
         """
         Finds surfaces touched if we put the lava droplet in water
         
-        position:tuple
-            current cube position
+        start:tuple
+            starting cube position
         lower_bounds:tuple
             Lower bounds of box, inclusive
         upper_bounds:tuple
             Upper bounds of box, inclusive
-        visited:set
-            Locations already assumed by cell
+        cube_centers:tuple
+            Cube centers.
         touched:set
             Tracks all faces we touch
+        
+        Returns:
+            Number of touched surfaces.
         """
-        # notes we've been here
-        visited.add(position)
+        # active positions
+        active = set()
+        active.add(start)
 
-        # checks if this cell touches any surface of cube
-        for i in range(3):
-            # we go by 1/2 because the center is 1/2 distance from everything,
-            # anything else would miscount. Float safe as power of 2
-            for num in {-1/2, 1/2}:
-                vector:list = list(position)
-                vector[i] += num
-                vector:tuple = tuple(vector)
-                # adds to touched if we touch a surface
-                if vector in surfaces:
-                    touched.add(vector)
+        # visited positions
+        visited:set = set()
+        visited |= cube_centers
 
-        # recursively calls to go to surrounding cells
-        for i in range(3):
-            for num in {-1, 1}:
-                modified_coord:int = position[i] + num
-                # if not in bounds, continue
-                if not (lower_bounds[i] <= modified_coord <= upper_bounds[i]):
-                    continue
+        # touched surfaces
+        touched:set = set()
 
-                # else, calculate new coordinate
-                vector:list = list(position)
-                vector[i] = modified_coord
-                vector:tuple = tuple(vector)
+        while len(active) > 0:
+            # gets an active position and removes from active
+            position = active.pop()
+            # notes we've been here, and block off the droplet itself
+            visited.add(position)
 
-                # if new coordinate has been visited or is solid, continue
-                if vector in cube_centers or vector in visited:
-                    continue
+            # checks if this cell touches any surface of cube
+            for i in range(3):
+                # we go by 1/2 because the center is 1/2 distance from everything,
+                # anything else would miscount. Float safe as power of 2
+                for num in {-1/2, 1/2}:
+                    vector:list = list(position)
+                    vector[i] += num
+                    vector:tuple = tuple(vector)
+                    # adds to touched if we touch a surface
+                    if vector in surfaces:
+                        touched.add(vector)
 
-                # repeat for new coord
-                water_flow(vector)
+            # calculates valid surrounding cells to flow to
+            for i in range(3):
+                for num in {-1, 1}:
+                    modified_coord:int = position[i] + num
+                    # if not in bounds, continue
+                    if not (lower_bounds[i] <= modified_coord <= upper_bounds[i]):
+                        continue
 
-    water_flow((0, 0, 0))
+                    # else, calculate new coordinate
+                    vector:list = list(position)
+                    vector[i] = modified_coord
+                    vector:tuple = tuple(vector)
+
+                    # if new coordinate has been visited or is solid, continue
+                    if vector in visited:
+                        continue
+
+                    # adds valid cell to active
+                    active.add(vector)
+        
+        return len(touched)
     
-    touched_surface_area:int = len(touched)
+    touched_surface_area:int = water_flow((0, 0, 0))
     print(touched_surface_area)
     copy_ans(touched_surface_area)
 
